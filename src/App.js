87232ba710b13b0a57570a78ed97f619f6a1e252
Venkata <rvsreddy.engg@gmail.com>
2019-03-14 21:02:02 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import form1 from './validation/form1';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Baisc form validation</h1>
+      <form1 />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/validation/form1.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class form1 extends React.Component {
+    constructor(props) {
+        super (props)
+        this.state= {
+            name: '',
+            email: '',
+            password: '',
+        };
+
+        handleChange= event => {
+            const isCheckbox = event.target.type === "checkbox";
+            this.setState({
+                [event.target.name]: isCheckbox
+                ? event.target.checked
+                : event.target.value
+            });
+        };
+
+        handleSubmit = event => {
+            event.preventDefault();
+            console.log(this.state);
+        };
+    }
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+            <div>
+              <input 
+                name='name'
+                placeholder='name'
+                value={this.state.name}
+                onChange={this.handleChange}
+                />
+            </div>
+
+            <div>
+              <input 
+                name='email'
+                placeholder='email'
+                value={this.state.email}
+                onChange={this.handleChange}
+                />
+            </div>
+
+            <div>
+              <input 
+                type='password'
+                name='password'
+                placeholder='password'
+                value={this.state.password}
+                onChange={this.handleChange}
+                />
+            </div>
+            <button type='submit'> Submit</button>
+
+            </form>
+            
+
+        );
+    }
+}
+
+export default form1;
